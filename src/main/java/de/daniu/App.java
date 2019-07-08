ba4a92bcd656752218deca5a8ce826f38206e883
joern <joern.schumacher@acando.de>
2019-07-08 16:36:20 +0200

    adding save (full cube visualcube query to text file)

--- src/main/java/de/daniu/App.java
+++ src/main/java/de/daniu/App.java
@@ -2,6 +2,11 @@ package de.daniu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.function.Supplier;
 
 import static java.util.stream.Collectors.joining;
 
@@ -31,14 +36,26 @@ public class App {
     }
 
     private static void layoutCommandContainer(JFrame frame, Cube cube, JComponent commandContainer) {
-        JButton okButton = new JButton("ok");
+        JButton okButton = new JButton("show");
         JTextField display = new JTextField("");
+        JButton saveButton = new JButton("save");
         commandContainer.add(okButton, BorderLayout.NORTH);
-        commandContainer.add(display, BorderLayout.SOUTH);
+        commandContainer.add(display, BorderLayout.CENTER);
+        commandContainer.add(saveButton, BorderLayout.SOUTH);
         frame.getContentPane().add(commandContainer, BorderLayout.SOUTH);
-        okButton.addActionListener(e -> {
-            String query = cube.getFaces().flatMap(CubeFace::colors).map(CubeColor::getQuery).collect(joining(""));
-            display.setText(query);
+        Supplier<String> cubeEncoder = () -> cube.getFaces().flatMap(CubeFace::colors).map(CubeColor::getQuery).collect(joining(""));
+        okButton.addActionListener(e -> display.setText(cubeEncoder.get()));
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        saveButton.addActionListener(e -> {
+            if (chooser.showSaveDialog(commandContainer) == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = chooser.getSelectedFile();
+                try (PrintWriter writer = new PrintWriter(new FileWriter(selectedFile, true))) {
+                    writer.println(cubeEncoder.get());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         });
     }
 
